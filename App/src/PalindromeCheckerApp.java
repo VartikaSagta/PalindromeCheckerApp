bc9c17491f9827f5c3130e18efe99eda5b36a33b
Vartika Sagta <vartikasagta@gmail.com>
2026-02-27 10:33:18 +0530

    Vartika Added Use Case 6 - Queue + Stack Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,24 +2,27 @@ import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        String input = "madam";
+        String input = "radar";
 
         System.out.println("==========================================");
-        System.out.println("Use Case 5 - Stack Based Palindrome Check");
+        System.out.println("Use Case 6 - Queue + Stack Palindrome Check");
         System.out.println("Input String : " + input);
 
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
-        // Push all characters into stack
+        // Insert characters into both structures
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            queue.add(ch);   // FIFO
+            stack.push(ch);  // LIFO
         }
 
         boolean isPalindrome = true;
 
-        // Compare original string with popped characters
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) != stack.pop()) {
+        // Compare elements
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
